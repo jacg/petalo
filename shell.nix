bd17eacb8071916cbf464f8d2e1fe074fffd43fe
Jacek Generowicz <jacg@my-post-office.net>
2020-10-20 12:32:03 +0200

    Refactor shell.nix and add Python

--- shell.nix
+++ shell.nix
@@ -3,8 +3,8 @@
 # commit that passes all the tests for any release. Unless there is an
 # overriding reason, pick the latest stable NixOS release, at the time of
 # writing this is nixos-20.09.
-{
- nixpkgs-commit-id ? "7badbf18c45b7490d893452beb8950d966327831" # nixos-20.09 on 2020-10-07
+{ nixpkgs-commit-id ? "7badbf18c45b7490d893452beb8950d966327831" # nixos-20.09 on 2020-10-07
+, py ? "38" # To override the default python version:  nix-shell shell.nix --argstr py 37
 }:
 let
   nixpkgs-url = "https://github.com/nixos/nixpkgs/archive/${nixpkgs-commit-id}.tar.gz";
@@ -13,49 +13,79 @@ let
         https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz
       ];
     };
-    rust-stable  = pkgs.latest.rustChannels.stable .rust;
-    rust-nightly = pkgs.latest.rustChannels.nightly.rust;
-    rust-specific = (pkgs.rustChannelOf { date = "2020-08-28"; channel = "nightly"; }).rust;
-    # to use the project's rust-toolchain file:
-    rust-toolchain = (pkgs.rustChannelOf { rustToolchain = ./rust-toolchain; }).rust;
-    buildInputs = [
-      (rust-stable.override {
-        extensions = [
-          "rust-analysis" # Rust Language Server (RLS)
-          "rust-src"      # Needed by RLS? or only rust-analyzer?
-          "rls-preview"   # What do *-preview offer?
-          "rustfmt-preview"
-          "clippy-preview"
-        ];
-      })
 
-      pkgs.pkgconfig
-
-      # for criterion plot generation
-      pkgs.gnuplot
-
-      # for kiss3d visualization
-      pkgs.xorg.libX11
-      pkgs.xorg.libXcursor
-      pkgs.xorg.libXrandr
-      pkgs.xorg.libXi
-      pkgs.libGL
-
-      # for plotters
-      pkgs.cmake
-      pkgs.freetype
-      pkgs.expat
-    ]
-    ++
-    pkgs.lib.optionals pkgs.stdenv.isDarwin [
-      pkgs.darwin.apple_sdk.frameworks.AppKit
-      pkgs.darwin.apple_sdk.frameworks.CoreText
+  # ----- Rust --------------------------------------------------------------------
+  rust-stable  = pkgs.latest.rustChannels.stable .rust;
+  rust-nightly = pkgs.latest.rustChannels.nightly.rust;
+  rust-specific = (pkgs.rustChannelOf { date = "2020-10-19"; channel = "nightly"; }).rust;
+  # to use the project's rust-toolchain file:
+  rust-toolchain = (pkgs.rustChannelOf { rustToolchain = ./rust-toolchain; }).rust;
+  # Rust system to be used in buldiInputs. Choose between
+  # stable/nightly/specific on the next line
+  rust = (rust-stable.override {
+    extensions = [
+      "rust-analysis" # Rust Language Server (RLS)
+      "rust-src"      # Needed by RLS? or only rust-analyzer?
+      "rls-preview"   # What do *-preview offer?
+      "rustfmt-preview"
+      "clippy-preview"
     ];
+  });
+
+  # ----- Python -------------------------------------------------------------------
+  python = builtins.getAttr ("python" + py) pkgs;
+  pypkgs = python.pkgs;
+
+  # ----- Conditional inclusion ----------------------------------------------------
+  nothing = pkgs.coreutils;
+  linux      = drvn: if pkgs.stdenv.isLinux  then drvn else nothing;
+  darwin     = drvn: if pkgs.stdenv.isDarwin then drvn else nothing;
+  linuxList  = list: if pkgs.stdenv.isLinux  then list else [];
+  darwinList = list: if pkgs.stdenv.isDarwin then list else [];
+
+  # ----- Darwin-specific ----------------------------------------------------------
+  darwin-frameworks = pkgs.darwin.apple_sdk.frameworks;
+
+  # --------------------------------------------------------------------------------
+  buildInputs = [
+    rust
+
+    pkgs.pkgconfig
+
+    # for criterion plot generation
+    pkgs.gnuplot
+
+    # for kiss3d visualization
+    pkgs.xorg.libX11
+    pkgs.xorg.libXcursor
+    pkgs.xorg.libXrandr
+    pkgs.xorg.libXi
+    pkgs.libGL
+
+    # for plotters
+    pkgs.cmake
+    pkgs.freetype
+    pkgs.expat
+
+    # profiling
+    (linux pkgs.linuxPackages.perf)
+    pkgs.cargo-flamegraph
+
+    # python
+    pypkgs.cffi
+    pypkgs.jupyter
+    pypkgs.matplotlib
+
+    # Needed for compilation to succeed on Macs
+    (darwin darwin-frameworks.AppKit)
+    (darwin darwin-frameworks.CoreText)
+
+  ];
 in
-  pkgs.stdenv.mkDerivation {
-    name = "rust";
-    buildInputs = buildInputs;
-    LD_LIBRARY_PATH = "${pkgs.stdenv.lib.makeLibraryPath buildInputs}";
-    # RA_LOG = "info";
-    # RUST_BAfCKTRACE = 1;
-  }
+pkgs.stdenv.mkDerivation {
+  name = "petalorust";
+  buildInputs = buildInputs;
+  LD_LIBRARY_PATH = "${pkgs.stdenv.lib.makeLibraryPath buildInputs}";
+  # RA_LOG = "info";
+  # RUST_BAfCKTRACE = 1;
+}
