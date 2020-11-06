2970506c61c0725f647ecebc7de0e2b97950ea77
Jacek Generowicz <jacg@my-post-office.net>
2020-11-06 17:40:48 +0100

    Use HDF5 files in vislor

--- src/bin/vislor.rs
+++ src/bin/vislor.rs
@@ -5,6 +5,7 @@ use petalo::weights::{VoxelBox, Length, LOR};
 use petalo::visualize::{lor_weights, Shape};
 
 use petalo::utils::{parse_triplet, parse_lor};
+use petalo::io;
 
 fn main() -> Result<(), Box<dyn Error>> {
 
@@ -18,8 +19,11 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // TODO: reading LOR from file overrides CLI lor: make them mutually
     // exclusive.
-    let lor = if let Some(file) = args.file {
-        petalo::io::bincode::read_lors(file)?[args.event]
+    let lor = if let Some(input_file) = args.clone().input_file {
+        let event_range = args.event..args.event+1;
+        let                      Cli{ dataset, use_true, .. } = args.clone();
+        let io_args = io::hdf5::Args{ dataset, use_true, input_file, event_range };
+        petalo::io::hdf5::read_lors(io_args)?[0]
     } else {
         args.lor
     };
@@ -30,7 +34,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 
-#[derive(StructOpt, Debug)]
+#[derive(StructOpt, Debug, Clone)]
 #[structopt(name = "petalo", about = "Visualize LOR interaction with voxels")]
 pub struct Cli {
 
@@ -47,9 +51,13 @@ pub struct Cli {
     #[structopt(possible_values = &Shape::variants(), case_insensitive = true, default_value = "box")]
     shape: Shape,
 
-    /// File containing LOR data
-    #[structopt(short, long)]
-    file: Option<std::path::PathBuf>,
+    /// LORs to read in
+    #[structopt(short = "f", long)]
+    pub input_file: Option<String>,
+
+    /// The dataset location inside the input file
+    #[structopt(short, long, default_value = "reco_info/table")]
+    pub dataset: String,
 
     /// Event number (in <file>) to be displayed
     #[structopt(short, long, default_value = "0")]
@@ -66,5 +74,9 @@ pub struct Cli {
     /// LOR to visualize: 't1 t2   x1 y1 z1   x2 y2 z2' (t: ps, xyz: mm)
     #[structopt(short, long, parse(try_from_str = parse_lor), default_value = "0 10  -100 20 -90  100 60 10")]
     lor: LOR,
+
+    /// Use true rather than reco LOR data from file
+    #[structopt(long)]
+    use_true: bool
 
 }
