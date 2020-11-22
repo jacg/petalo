116757af6934b73e3d3425cb31a0bfb0b279b565
Jacek Generowicz <jacg@my-post-office.net>
2020-11-22 16:51:41 +0100

    split(",") -> split(',')

--- src/utils.rs
+++ src/utils.rs
@@ -12,7 +12,7 @@ pub fn parse_range<T: std::str::FromStr>(s: &str) -> Result<Range<T>, <T as std:
 }
 
 pub fn parse_triplet<T: std::str::FromStr>(s: &str) -> Result<(T,T,T), <T as std::str::FromStr>::Err> {
-    let v = s.split(",").collect::<Vec<_>>();
+    let v = s.split(',').collect::<Vec<_>>();
     assert!(v.len() == 3);
     let x = v[0].parse()?;
     let y = v[1].parse()?;
