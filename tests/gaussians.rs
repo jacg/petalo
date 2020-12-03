b94119d6d13944dfe6a8eedb24f25bbac34ceeeb
Jacek Generowicz <jacg@my-post-office.net>
2020-12-03 09:58:45 +0100

    Fix gaussian equivalence

--- tests/gaussians.rs
+++ tests/gaussians.rs
@@ -6,12 +6,12 @@ use petalo::weights::{Length, Time, Ratio};
 // https://github.com/jerenner/tofpet3d/blob/a8c3fc8293fd05547f9ca752abc259173bac57af/cc/mlem.cc#L146-L163
 // in terms of petalo::weights::make_gauss
 #[allow(nonstandard_style)]
-fn sim_burdel(dist: Length, deltaT: Time, TOF_resolution: Time) -> Ratio {
+fn sim_burdel(dist: Length, dt: Time, sigma: Time) -> Ratio {
     use petalo::weights::{make_gauss_option, C};
-    let t = dist * 2.0 / C + deltaT ;
-    let sigma = TOF_resolution;
+    let x = dist + dt * C / 2.0;
     let cutoff = Some(3.0);
-    make_gauss_option(Some(sigma), cutoff).unwrap()(t) * 2.0 / C
+    let gauss = make_gauss_option(Some(sigma), cutoff).unwrap();
+    gauss(x)
 }
 
 // Check that sim_burdel is equivalent to ale_burdel a.k.a ToFFunction
@@ -26,6 +26,7 @@ proptest! {
         use cmlem::ale_burdel;
         let sane = sim_burdel(dist, dt, resolution);
         let mad  = ale_burdel(dist, dt, resolution);
+        println!("ratio: {}", sane / mad);
         assert_approx_eq!(sane, mad);
     }
 }
