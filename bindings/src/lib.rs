3a95a78fe470979cb557c4722a0e329c3a515698
Jacek Generowicz <jacg@my-post-office.net>
2020-12-07 11:23:36 +0100

    Add most basic PyO3 module containing one function

---
+++ bindings/src/lib.rs
@@ -0,0 +1,15 @@
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+
+
+#[pyfunction]
+fn fib(n: usize) -> usize {
+    if n < 2 { 1 }
+    else     { fib(n-1) + fib(n-2) }
+}
+
+#[pymodule]
+fn fulano(_python_gil: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(fib, m)?)?;
+    Ok(())
+}
