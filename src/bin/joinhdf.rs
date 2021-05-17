4fbb9be87b2f58cb2e202a86da490d19de883f2e
Jacek Generowicz <jacg@my-post-office.net>
2021-05-17 15:23:08 +0200

    Add joinhdf.rs

---
+++ src/bin/joinhdf.rs
@@ -0,0 +1,53 @@
+use structopt::StructOpt;
+use petalo::io;
+use petalo::io::hdf5::Hdf5Lor;
+
+#[derive(StructOpt, Debug, Clone)]
+#[structopt(name = "joinhdf", about = "Combine datasets from separate HDF5 files into a single one")]
+pub struct Cli {
+    /// HDF5 input file with (event_id, sensor_id, Q, t0)
+    pub inputs: Vec<String>,
+
+    /// HDF5 output file for LORs found in input file
+    #[structopt(short, long)]
+    pub outfile: String,
+
+    /// Group containing the dataset in each file
+    #[structopt(short, long)]
+    pub group: String,
+
+    /// Dataset to read from each input file
+    #[structopt(short, long)]
+    pub dataset: String,
+
+    // TODO allow using different group/dataset in output
+}
+
+
+type Data = Hdf5Lor; // TODO: add CLI switches for selecting type
+
+
+fn main() -> hdf5::Result<()> {
+    let args = Cli::from_args();
+    let mut joined = Vec::<Data>::new();
+
+    // ----- read data from separate files -------------------------------------------
+    for filename in args.inputs {
+        println!("Reading data from {}", filename);
+        let path = format!("{}/{}", args.group, args.dataset);
+        let mut data = io::hdf5::read_table::<Data>(&filename, &path, None)?;
+        joined.extend_from_slice(data.as_slice_mut().unwrap());
+        // TODO ndarray 0.14 -> 0.15: breaks our code in hdf5
+        // joined.extend_from_slice(data.into_slice());
+    }
+
+    // --- write combined data to single file ----------------------------------------
+    let outname = args.outfile;
+    println!("Writing data to {}", outname);
+    hdf5::File::create(outname)?
+        .create_group(&args.group)?
+        .new_dataset::<Data>().create(&args.dataset, joined.len())?
+        .write(&joined)?;
+
+    Ok(())
+}
