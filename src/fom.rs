29832bf5df812543fea3d5c7173df8f2a6202789
Jacek Generowicz <jacg@my-post-office.net>
2021-05-31 12:21:10 +0200

    Implement and test Cylindrical ROI

--- src/fom.rs
+++ src/fom.rs
@@ -29,9 +29,16 @@ mod test_get_sample_image {
 }
 
 
+
+type Sphere = ((Length, Length, Length), Length);
+type Cylinder = ((Length, Length), Length);
+
+// TODO replace vec with iterator in output
 impl Image {
-    pub fn values_inside_sphere(&self, (cx, cy, cz): (Length, Length, Length), radius: Length) -> ImageData {
+
+    pub fn values_inside_sphere(&self, sphere: Sphere) -> ImageData {
         let mut out = vec![];
+        let ((cx, cy, cz), radius) = sphere;
         for (index, value) in self.data.iter().copied().enumerate() {
             let p = self.vbox.voxel_centre1(index);
             let (x, y, z) = (p.x - cx, p.y - cy, p.z - cz);
@@ -40,12 +47,28 @@ impl Image {
         }
         out
     }
+
+    pub fn values_inside_cylinder(&self, cylinder: Cylinder) -> ImageData {
+        let mut out = vec![];
+        let ((cx, cy), radius) = cylinder;
+        for (index, value) in self.data.iter().copied().enumerate() {
+            let p = self.vbox.voxel_centre1(index);
+            let (x, y) = (p.x - cx, p.y - cy);
+            let d_squared = x*x + y*y;
+            if d_squared < radius*radius { out.push(value); }
+        }
+        out
+    }
+
 }
 
 #[cfg(test)]
 mod test_in_sphere {
     use super::*;
     use rstest::rstest;
+
+    // TODO these tests only verify the number of included voxels but do not
+    // check whether the correct ones have been included
 
     // Arrange for outer voxels to be centred at +/- 100.0, when n = 10
     const MAGIC: Length = 10.0 / 9.0 * 200.0;
@@ -57,17 +80,41 @@ mod test_in_sphere {
              case(MAGIC, 10, (200.0, 200.0, 200.0), 173.3,    1), // single voxel at one corner of box
              case(MAGIC, 10, (200.0, 200.0, 200.0), 173.2,    0), // slightly smaller r excludes the corner
     )]
-    fn number_of_included_voxels(l: Length,
-                                 n: usize,
-                                 centre: (Length, Length, Length),
-                                 r: Length,
-                                 expected_len: usize
+    fn number_of_included_voxels_in_sphere(
+        l: Length,
+        n: usize,
+        centre: (Length, Length, Length),
+        r: Length,
+        expected_len: usize
     ) {
         let data = vec![1.0; n*n*n];
         let vbox = VoxelBox::new((l,l,l), (n,n,n));
         let image = Image::new(vbox, data);
-        let inside = image.values_inside_sphere(centre, r);
+        let inside = image.values_inside_sphere((centre, r));
         println!("{} {}", inside.len(), expected_len);
         assert!(inside.len() == expected_len);
     }
+
+    #[rstest(/**/    l ,  n,        centre        ,    r , expected_len,
+             case(MAGIC, 10, (  0.0,   0.0), 141.5, 1000), // r > sqrt(2) * 100; all voxel centres inside sphere
+             case(MAGIC, 10, (  0.0,   0.0), 141.4,  960), // r < sqrt(2) * 100; 960 = 4*n corners missing
+             case(MAGIC,  9, (  0.0,   0.0), 141.4,  729), // all 9^3 included: coarser grid => outer centres closer
+             case(MAGIC, 10, (200.0, 200.0), 141.5,   10), // one row of voxels at one corner of box
+             case(MAGIC, 10, (200.0, 200.0), 141.4,    0), // slightly smaller r excludes the corner
+    )]
+    fn number_of_included_voxels_in_z_cylinder(
+        l: Length,
+        n: usize,
+        centre: (Length, Length),
+        r: Length,
+        expected_len: usize
+    ) {
+        let data = vec![1.0; n*n*n];
+        let vbox = VoxelBox::new((l,l,l), (n,n,n));
+        let image = Image::new(vbox, data);
+        let inside = image.values_inside_cylinder((centre, r));
+        println!("{} {}", inside.len(), expected_len);
+        assert!(inside.len() == expected_len);
+    }
+
 }
