ea0c09be7a10ff0401f5a5979a1f782d5cd19d7c
Jacek Generowicz <jacg@my-post-office.net>
2021-06-02 13:03:24 +0200

    Add basic pytests

--- bindings/README.md
+++ bindings/README.md
@@ -4,7 +4,4 @@
 
 + cd into this directory
 + `cargo build --release`
-+ `cd ..`
-+ `ln -s target/release/libfulano.so libfulano.so`
-+ `python`
-+ `import fulano`
++ `pytest`
---
+++ bindings/bindings_test.py
@@ -0,0 +1,24 @@
+import fulano
+
+def test_fib():
+    assert fulano.fib(10) == 89
+
+def test_fab():
+    assert fulano.fab(10) == 89
+
+def test_lift():
+    l = fulano.Lift(10)
+    assert l.height == 10
+    l.up(2)
+    assert l.height == 12
+    l.down(10)
+    assert l.height ==  2
+
+def test_fib_doc():
+    assert fulano.fib.__doc__ == 'The naive, recursive fibonacci implementation'
+
+def test_fab_doc():
+    assert fulano.fab.__doc__ == 'The iterative fibonacci implementation'
+
+def test_Lift_doc():
+    assert fulano.Lift.__doc__ == "It's a Lift: it goes up and down"
