b3319fdd0eb3df66935afab2e32efaa3228e7d2c
Jacek Generowicz <jacg@my-post-office.net>
2021-06-18 09:47:06 +0200

    Test simplest binread example

--- src/io/raw.rs
+++ src/io/raw.rs
@@ -60,3 +60,31 @@ mod test {
         Ok(())
     }
 }
+
+
+use binread::{BinRead, BinReaderExt, NullString, io::Cursor};
+
+#[derive(BinRead)]
+#[br(magic = b"DOG", assert(name.len() != 0))]
+struct Dog {
+    bone_pile_count: u8,
+
+    #[br(big, count = bone_pile_count)]
+    bone_piles: Vec<u16>,
+
+    #[br(align_before = 0xA)]
+    name: NullString
+}
+
+#[cfg(test)]
+mod test_binrw {
+    use super::*;
+
+    #[test]
+    fn simplest() {
+        let mut reader = Cursor::new(b"DOG\x02\x00\x01\x00\x12\0\0Rudy\0");
+        let dog: Dog = reader.read_ne().unwrap();
+        assert_eq!(dog.bone_piles, &[0x1, 0x12]);
+        assert_eq!(dog.name.into_string(), "Rudy")
+    }
+}
