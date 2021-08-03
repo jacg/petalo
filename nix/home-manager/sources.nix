f76ddad298c86d3dde2e47ae61f26d0d75826619
Jacek Generowicz <jacg@my-post-office.net>
2021-08-03 20:56:11 +0200

    Bump NixOS version

--- nix/home-manager/sources.nix
+++ nix/home-manager/sources.nix
@@ -1,13 +1,13 @@
 # To get a more recent version of nixpkgs, go to https://status.nixos.org/,
 # which lists the latest commit that passes all the tests for any release.
 # Unless there is an overriding reason, pick the latest stable NixOS release, at
-# the time of writing this is nixos-20.09.
+# the time of writing this is nixos-21.05.
 
 { pkgs }:
 
 let
   random_pkgs = import <nixpkgs> {};
-  nixpkgs-commit-id = "ea7d4aa9b8225abd6147339f0d56675d6f1f0fd1"; # nixos-unstable on 2021-05-25
+  nixpkgs-commit-id = "d4590d21006387dcb190c516724cb1e41c0f8fdf"; # nixos-21.05 on 2021-08-03
   nixpkgs-url = "https://github.com/nixos/nixpkgs/archive/${nixpkgs-commit-id}.tar.gz";
   pkgs = import (fetchTarball nixpkgs-url) {
       overlays = map (uri: import (fetchTarball uri)) [
