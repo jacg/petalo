b339f81a5d284e11693e0e7b926aa7916946c881
Jacek Generowicz <jacg@my-post-office.net>
2021-08-12 10:07:50 +0200

    Pin oxalica rust overlay

--- nix/home-manager/sources.nix
+++ nix/home-manager/sources.nix
@@ -9,9 +9,10 @@ let
   random_pkgs = import <nixpkgs> {};
   nixpkgs-commit-id = "d4590d21006387dcb190c516724cb1e41c0f8fdf"; # nixos-21.05 on 2021-08-03
   nixpkgs-url = "https://github.com/nixos/nixpkgs/archive/${nixpkgs-commit-id}.tar.gz";
+  oxalica-commit-id = "53b758596e8bd17e9fc764e14ba3e7686030bad3";
   pkgs = import (fetchTarball nixpkgs-url) {
       overlays = map (uri: import (fetchTarball uri)) [
-        https://github.com/oxalica/rust-overlay/archive/master.tar.gz
+        "https://github.com/oxalica/rust-overlay/archive/${oxalica-commit-id}.tar.gz"
       ];
     };
 in
