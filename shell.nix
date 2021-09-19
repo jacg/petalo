ddca9e709c9d8fe02e060a0449943717d3f023c7
Jacek Generowicz <jacg@my-post-office.net>
2021-09-19 18:38:10 +0200

    Rust -> 1.55.0

--- shell.nix
+++ shell.nix
@@ -25,9 +25,9 @@ let
   rust-tcfile  = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain;
 
   rust-latest  = pkgs.rust-bin.stable .latest      .default;
-  rust-beta    = pkgs.rust-bin.beta   ."2021-08-28".default;
-  rust-nightly = pkgs.rust-bin.nightly."2021-08-28".default;
-  rust-stable  = pkgs.rust-bin.stable ."1.54.0"    .default;
+  rust-beta    = pkgs.rust-bin.beta   ."2021-09-18".default;
+  rust-nightly = pkgs.rust-bin.nightly."2021-09-18".default;
+  rust-stable  = pkgs.rust-bin.stable ."1.55.0"    .default;
 
   # Rust system to be used in buldiInputs. Choose between
   # latest/beta/nightly/stable on the next line
