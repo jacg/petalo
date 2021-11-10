96a02f36383417500e7070379f575e23cf1d44b4
Jacek Generowicz <jacg@my-post-office.net>
2021-11-10 13:29:58 +0100

    Add usage instructions to add_raw_header.py

--- python/add_raw_header.py
+++ python/add_raw_header.py
@@ -2,7 +2,20 @@ from sys   import argv
 from utils import read_raw
 import struct
 
-_, nx,ny,nz, dx,dy,dz, *filenames = argv
+try:
+    prog, nx,ny,nz, dx,dy,dz, *filenames = argv
+except ValueError:
+    exit(f"""Add size information headers to 3D raw image files
+
+    Usage:
+
+       {argv[0]}  NX NY NZ  DX DY DZ  FILENAMES...
+
+    NX, NY, NZ: number of voxels in each dimension
+    DX, DY, DZ: full extent of FOV in each dimension
+
+    The original files will not be modified. The headers will be added to new
+    files matching the original names with the suffix `_h`""")
 
 nx,ny,nz = map(int  , (nx,ny,nz))
 dx,dy,dz = map(float, (dx,dy,dz))
