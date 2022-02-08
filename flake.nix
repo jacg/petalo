44648003e60fe95fc4bf458edb3e5c77ef2b351c
Jacek Generowicz <jacg@my-post-office.net>
2022-02-08 17:18:01 +0100

    Replace local flake with flake from template

--- flake.nix
+++ flake.nix
@@ -1,172 +1,101 @@
-# The core functionality is provided here using flakes. Legacy support for
-# `nix-shell` is provided by a wrapper in `shell.nix`.
-
-# TODO HDF5
-# TODO bindgen
-# TODO PyO3
-# TODO nixGL
-
 {
-  description = "PET image reconstruction";
+  description = "Nixified Rust project";
 
   inputs = {
-
-    # Version pinning is managed in flake.lock. Upgrading can be done with
-    # something like
-    #
-    #    nix flake lock --update-input nixpkgs
-
-    nixpkgs     .url = "github:nixos/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils .url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
-
+    nixpkgs         .url = "github:nixos/nixpkgs/nixos-unstable";
+    utils           .url = "github:numtide/flake-utils";
+    rust-overlay = { url = "github:oxalica/rust-overlay"; inputs.nixpkgs    .follows = "nixpkgs";
+                                                          inputs.flake-utils.follows = "utils"; };
+    crate2nix    = { url = "github:kolloch/crate2nix";     flake = false; };
+    flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-
-    # Option 1: try to support each default system
-    flake-utils.lib.eachDefaultSystem # NB Some packages in nixpkgs are not supported on some systems
-
-    # Option 2: try to support selected systems
-    # flake-utils.lib.eachSystem ["x86_64-linux" "i686-linux" "aarch64-linux" "x86_64-darwin"]
+  outputs = { self, nixpkgs, utils, rust-overlay, crate2nix, ... }:
+    let
+      # This name must match the name in Cargo.toml
+      name = "nixified-rust-project";
+    in
+    utils.lib.eachDefaultSystem
       (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              # ===== Specification of the rust toolchain to be used ====================
+              rust-overlay.overlay (final: prev:
+                let
+                  # If you have a rust-toolchain file for rustup, choose `rustup =
+                  # rust-tcfile` further down to get the customized toolchain
+                  # derivation.
+                  rust-tcfile  = final.rust-bin.fromRustupToolchainFile ./rust-toolchain;
+                  rust-latest  = final.rust-bin.stable .latest      ;
+                  rust-beta    = final.rust-bin.beta   .latest      ;
+                  rust-nightly = final.rust-bin.nightly."2022-02-07";
+                  rust-stable  = final.rust-bin.stable ."1.58.1"    ; # nix flake lock --update-input rust-overlay
+                  rust-analyzer-preview-on = date:
+                    final.rust-bin.nightly.${date}.default.override
+                      { extensions = [ "rust-analyzer-preview" ]; };
+                in
+                  rec {
+                    # The version of the Rust system to be used in buildInputs. Choose between
+                    # tcfile/latest/beta/nightly/stable (see above) on the next line
+                    rustup = rust-stable;
 
-        let pkgs = import nixpkgs {
-              inherit system;
-              # Any overlays you need can go here
-              overlays = [ (import rust-overlay) ];
-            };
-            # ----- Conditional inclusion ----------------------------------------------------
-            nothing = pkgs.coreutils;
-            linux      = drvn: if pkgs.stdenv.isLinux  then drvn else nothing;
-            darwin     = drvn: if pkgs.stdenv.isDarwin then drvn else nothing;
-            linuxList  = list: if pkgs.stdenv.isLinux  then list else [];
-            darwinList = list: if pkgs.stdenv.isDarwin then list else [];
-            darwinStr  = str:  if pkgs.stdenv.isDarwin then str  else "";
-
-            # ----- Darwin-specific ----------------------------------------------------------
-            darwin-frameworks = pkgs.darwin.apple_sdk.frameworks;
-
-            # ----- Rust versions and extensions ----------------------------------
-            extras = {
-              extensions = [
-
-                # Uncomment the extensions you want to enable, in the list below.
-                # Some are enabled by default.
-
-                # A history of available extensions is published on
-                #
-                #    https://rust-lang.github.io/rustup-components-history
-                #
-                # but this appears to contradict reality. To get an error message
-                # which lists all available components, uncomment the following
-                # line
-
-                #  "does-not-exist" # Uncomment this line to generate list of extensions
-
-                # Extensions which might be available. Uncomment those you want to
-                # enable. Some are enabled by default.
-
-                # "cargo"                    # enabled by default
-                # "clippy"                   # enabled by default
-                # "clippy-preview"           # seems to give the same version as clippy
-                # "llvm-tools-preview"
-                # "miri"
-                # "miri-preview"
-                # "reproducible-artifacts"
-                # "rls"
-                # "rls-preview"              # seems to give the same version as rls
-                # "rust"
-                # "rust-analysis"
-                # "rust-analyzer-preview"
-                # "rust-docs"
-                # "rust-mingw"
-                # "rust-src"
-                # "rust-std"
-                # "rustc"
-                # "rustc-dev"
-                # "rustc-docs"
-                # "rustfmt"
-                # "rustfmt-preview"
-              ];
-              #targets = [ "arg-unknown-linux-gnueabihf" ];
-            };
-
-            # If you already have a rust-toolchain file for rustup, you can simply
-            # use fromRustupToolchainFile to get the customized toolchain
-            # derivation.
-            rust-tcfile  = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain;
-
-            rust-latest  = pkgs.rust-bin.stable .latest      .default;
-            rust-beta    = pkgs.rust-bin.beta   ."2022-01-25".default;
-            rust-nightly = pkgs.rust-bin.nightly."2022-01-25".default;
-            rust-stable  = pkgs.rust-bin.stable ."1.58.1"    .default;
-
-            # Rust system to be used in buldiInputs. Choose between
-            # latest/beta/nightly/stable on the next line
-            rust = rust-stable.override extras;
-
-            mkBuildInputs = { pkgs, pythonVersion, rust }: [
-              rust
-              pkgs.rust-analyzer
-              pkgs.just
-
-              # HDF5
-              pkgs.hdf5
-
-              # Needed for compilation to succeed on Macs
-              (darwin darwin-frameworks.AppKit)
-              (darwin darwin-frameworks.CoreText)
-
-              # python
-              (pkgs.${ pythonVersion  }.withPackages (ps: [
-                ps.numpy
-                ps.cffi
-                ps.jupyter
-                ps.matplotlib
-                ps.pytest
-                ps.h5py
-                ps.scikit-learn
-                ps.docopt
-              ]))
-
-              # julia
-              (linux pkgs.julia_16-bin)
-
+                    rustc = rustup.default;
+                    cargo = rustup.default;
+                    rust-analyzer-preview = rust-analyzer-preview-on "2022-02-07";
+                  })
             ];
+          };
+          inherit (import "${crate2nix}/tools.nix" { inherit pkgs; }) generatedCargoNix;
 
+          # Create the cargo2nix project
+          project = pkgs.callPackage
+            (generatedCargoNix {
+              inherit name;
+              src = ./.;
+            })
+            {
+              # See "Handling external dependencies" on
+              # https://ryantm.github.io/nixpkgs/languages-frameworks/rust/#rust
+              defaultCrateOverrides = pkgs.defaultCrateOverrides // {
+                ${name} = old-attributes: {
+                  inherit buildInputs nativeBuildInputs;
+                };
+              };
+            };
+
+          # non-Rust dependencies
+          buildInputs = [ pkgs.openssl.dev ];
+          nativeBuildInputs = [ pkgs.rustc pkgs.cargo ];
         in
-          {
-            devShell = self.devShells.${ system }.python39; # does not need `rec`
-            #devShell = devShells.python39;                 # simpler but needs `rec`
+        rec {
+          packages.${name} = project.rootCrate.build;
 
-            devShells =
-              builtins.listToAttrs (
-                builtins.map (
-                  pythonVersion: {
+          # ========== nix build =========================================================
+          defaultPackage = packages.${name};
 
-                    name = pythonVersion;
-                    value = pkgs.mkShell {
-                      name = "petalorust";
-                      buildInputs = mkBuildInputs { inherit pkgs pythonVersion rust; };
-                      packages = [
-                      ];
-                      HDF5_DIR = pkgs.symlinkJoin { name = "hdf5"; paths = [ pkgs.hdf5 pkgs.hdf5.dev ]; };
-                      shellHook =
-                        ''
-                          export PS1="rust devshell> "
-                          alias foo='cowsay Foo'
-                          alias bar='exa -l | lolcat'
-                          alias baz='cowsay What is the difference between buildIntputs and packages? | lolcat'
-                '';
-                    };
-                  }
-                ) [ "python37" "python38" "python39" "python310" ]
-              );
-          }
+          # ========== nix run ============================================================
+          defaultApp = apps.${name};
+
+          apps.${name} = utils.lib.mkApp {
+            inherit name;
+            drv = packages.${name};
+          };
+
+          apps.one = utils.lib.mkApp { drv = packages.${name}; name = "one"; };
+          apps.two = utils.lib.mkApp { drv = packages.${name}; name = "two"; };
+
+          # ========== nix develop ========================================================
+          devShell = pkgs.mkShell {
+            inputsFrom = builtins.attrValues self.packages.${system};
+            buildInputs = buildInputs ++ [
+              # Tools you need for development go here.
+              pkgs.rust-analyzer-preview
+              #pkgs.rustup.rls pkgs.rustup.rust-analysis
+            ];
+            RUST_SRC_PATH = "${pkgs.rustup.rust-src}/lib/rustlib/src/rust/library";
+          };
+        }
       );
 }
