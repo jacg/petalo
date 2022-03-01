2f806ef4aea5c4d76f65f1b16ad73e6f8d7d4a8b
Jacek Generowicz <jacg@my-post-office.net>
2022-03-01 13:20:40 +0100

    Prevent accidental reuse of similarly-named variables

--- src/bin/show_lorogram.rs
+++ src/bin/show_lorogram.rs
@@ -31,51 +31,57 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let infile  = args.input_file.into_os_string().into_string().unwrap();
 
+    {
+        println!("===== z dependence ======================================");
+        let lors = read_table::<Hdf5Lor>(&infile, &args.dataset, args.event_range.clone())?;
+        let nbins = 20;
+        let l = 200.0;
+        let sgram = fill_scattergram(JustZ::new(l, nbins), lors);
+        let l0 = - l / 2.0;
+        let dl = l / (nbins as f32);
 
-    println!("===== z dependence ======================================");
-    let lors = read_table::<Hdf5Lor>(&infile, &args.dataset, args.event_range.clone())?;
-    let nbins = 20;
-    let l = 200.0;
-    let sgram = fill_scattergram(JustZ::new(l, nbins), lors);
-    let l0 = - l / 2.0;
-    let dl = l / (nbins as f32);
-
-    for i in 0..nbins {
-        let z = l0 + (i as f32 + 0.5) * dl;
-        let p = (0.0, 0.0, z as f32);
-        let (v, t, s) = sgram.triplet(p, p);
-        println!("{z:7.1}   {v:10.2}    {t:8}  {s:8}");
+        println!("     z       (s/t) + 1     trues   scatters");
+        for i in 0..nbins {
+            let z = l0 + (i as f32 + 0.5) * dl;
+            let p = (0.0, 0.0, z as f32);
+            let (v, t, s) = sgram.triplet(p, p);
+            println!("{z:7.1}   {v:10.2}    {t:8}  {s:8}");
+        }
     }
+    {
+        println!("===== phi dependence ====================================");
+        let lors = read_table::<Hdf5Lor>(&infile, &args.dataset, args.event_range.clone())?;
+        let nbins = 15;
+        let sgram = fill_scattergram(JustPhi::new(nbins), lors);
 
-    println!("===== phi dependence ====================================");
-    let lors = read_table::<Hdf5Lor>(&infile, &args.dataset, args.event_range.clone())?;
-    let n = 15;
-    let sgram = fill_scattergram(JustPhi::new(n), lors);
-
-    for i in 0..n {
-        let phi = PI * ((i as f32 + 0.5) / n as f32);
-        let x = phi.cos();
-        let y = phi.sin();
-        //println!("{:10.1} {:10.1} {:10.1} {:10.1} {}", x, y, phi, phi/PI, i);
-        let p1 = (x, 0.0, 0.0);
-        let p2 = (0.0, y, 0.0);
-        let (v, t, s) = sgram.triplet(p1, p2);
-        let phi = phi * 180.0 / PI;
-        println!("{phi:7.1}   {v:10.2}    {t:8}  {s:8}");
+        println!("   phi       (s/t) + 1     trues   scatters");
+        for i in 0..nbins {
+            let phi = PI * ((i as f32 + 0.5) / nbins as f32);
+            let x = phi.cos();
+            let y = phi.sin();
+            let p1 = (x, 0.0, 0.0);
+            let p2 = (0.0, y, 0.0);
+            let (v, t, s) = sgram.triplet(p1, p2);
+            let phi = phi * 180.0 / PI;
+            println!("{phi:7.1}   {v:10.2}    {t:8}  {s:8}");
+        }
     }
-
-    println!("===== r dependence ====================================");
-    let lors = read_table::<Hdf5Lor>(&infile, &args.dataset, args.event_range)?;
-    let nbins = 15;
-    let r_max = 120.0;
-    let sgram = fill_scattergram(JustR::new(r_max, nbins), lors);
-    let step = r_max / nbins as f32;
-    for i in 0..nbins {
-        let r = (i as f32 + 0.5) * step;
-        let p1 = (r,  100.0, 0.0);
-        let p2 = (r, -100.0, 0.0);
-        let (v, t, s) = sgram.triplet(p1, p2);
-        println!("{r:7.1}   {v:10.2}    {t:8}  {s:8}");
+    {
+        println!("===== r dependence ====================================");
+        let lors = read_table::<Hdf5Lor>(&infile, &args.dataset, args.event_range.clone())?;
+        let nbins = 15;
+        let r_max = 120.0;
+        let sgram = fill_scattergram(JustR::new(r_max, nbins), lors);
+        let step = r_max / nbins as f32;
+        println!("     r       (s/t) + 1     trues   scatters");
+        for i in 0..nbins {
+            let r = (i as f32 + 0.5) * step;
+            let p1 = (r,  100.0, 0.0);
+            let p2 = (r, -100.0, 0.0);
+            let (v, t, s) = sgram.triplet(p1, p2);
+            println!("{r:7.1}   {v:10.2}    {t:8}  {s:8}");
+        }
+    }
     }
 
     Ok(())
