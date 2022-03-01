e3edae55613b2d661430e4bb18a41a7d556e33cb
Jacek Generowicz <jacg@my-post-office.net>
2022-03-01 09:52:44 +0100

    Include raw counts in output

--- src/bin/show_lorogram.rs
+++ src/bin/show_lorogram.rs
@@ -38,8 +38,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     for z in (-525..=525).step_by(50) {
         let p = (0.0, 0.0, z as f32);
-        let v = sgram.value(p, p);
-        println!("{z:5}   {v:3.1}");
+        let (v, t, s) = sgram.triplet(p, p);
+        println!("{z:5}   {v:10.2}    {t:8}  {s:8}");
     }
 
     println!("===== phi dependence ====================================");
@@ -54,9 +54,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         //println!("{:10.1} {:10.1} {:10.1} {:10.1} {}", x, y, phi, phi/PI, i);
         let p1 = (x, 0.0, 0.0);
         let p2 = (0.0, y, 0.0);
-        let v = sgram.value(p1, p2);
+        let (v, t, s) = sgram.triplet(p1, p2);
         let phi = phi * 180.0 / PI;
-        println!("{phi:5.1}   {v:3.1}");
+        println!("{phi:5.1}   {v:10.2}    {t:8}  {s:8}");
     }
 
     println!("===== r dependence ====================================");
@@ -70,7 +70,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let p1 = (r,  100.0, 0.0);
         let p2 = (r, -100.0, 0.0);
         let (v, t, s) = sgram.triplet(p1, p2);
-        println!("{r:5.1}   {v:5.2}    {t:8}  {s:8}");
+        println!("{r:7.1}   {v:10.2}    {t:8}  {s:8}");
     }
 
     Ok(())
@@ -82,7 +82,7 @@ fn fill_scattergram<T: Lorogram + Clone>(prototype: T, lors: ndarray::Array1<Hdf
         if x1.is_nan() || x2.is_nan() { continue }
         let p1 = (x1, y1, z1);
         let p2 = (x2, y2, z2);
-        let prompt = if E1.max(E2) < 511.0 { Prompt::Scatter } else { Prompt::True };
+        let prompt = if E1.min(E2) < 511.0 { Prompt::Scatter } else { Prompt::True };
         sgram.fill(prompt, p1, p2);
     }
     sgram
