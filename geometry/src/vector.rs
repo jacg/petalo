134691d2999d49d40dbe119319fcdd7f60518c0b
Jacek Generowicz <jacg@my-post-office.net>
2022-03-16 16:25:38 +0100

    Test Mul<f32> for Vector

--- geometry/src/vector.rs
+++ geometry/src/vector.rs
@@ -1,5 +1,5 @@
 use std::ops::{Index, Mul};
-use uom::si::f32::{Length, Ratio};
+use uom::si::f32::Length;
 
 //use crate::Point;
 
@@ -12,9 +12,9 @@ pub struct Vector {
 
 
 
-impl Mul<Ratio> for Vector {
+impl Mul<f32> for Vector {
     type Output = Self;
-    fn mul(self, rhs: Ratio) -> Self::Output {
+    fn mul(self, rhs: f32) -> Self::Output {
         Vector {
             x: self.x * rhs,
             y: self.y * rhs,
@@ -71,5 +71,15 @@ mod tests {
         assert_eq!(           v.x, mm(10.0  ));
         assert_uom_eq!(meter, v.y, mm( 0.001), r2nd <= EPS);
         assert_uom_eq!(meter, v.z, cm( 0.2  ), r2nd <= EPS);
+    }
+
+    #[test]
+    fn mul_f32_for_vector() {
+        let v = Vector::new(mm(1.0), mm(2.0), mm(3.0));
+        let e = Vector::new(cm(1.0), cm(2.0), cm(3.0));
+        let r = v * 10.0;
+        assert_uom_eq!(meter, r.x, e.x, ulps <= 1);
+        assert_uom_eq!(meter, r.y, e.y, ulps <= 1);
+        assert_uom_eq!(meter, r.z, e.z, ulps <= 1);
     }
 }
