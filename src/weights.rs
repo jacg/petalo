ae2e13d85d0caec5e3a8b3741c6fa0e01af4bb45
Jacek Generowicz <jacg@my-post-office.net>
2022-04-05 13:09:00 +0200

    Remove LENGTH_ZERO

--- src/weights.rs
+++ src/weights.rs
@@ -283,7 +283,6 @@ pub fn system_matrix_elements(
 }
 
 use crate::types::guomc::ConstZero;
-const LENGTH_ZERO: Length = Length::ZERO;
 const F32_LENGTH_ZERO: Lengthf32 = 0.0;
 
 /// The point at which the LOR enters the FOV, expressed in a coordinate
