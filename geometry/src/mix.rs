bd2585059dc3061c57dce5c1469aceb7d9b21905
Jacek Generowicz <jacg@my-post-office.net>
2022-04-05 23:45:37 +0200

    Remove superfluous converters from mix.rs

--- geometry/src/mix.rs
+++ geometry/src/mix.rs
@@ -46,42 +46,12 @@ impl From<Vector> for ncollide3d::math::Vector<f32> {
     }
 }
 
-impl From<&ncollide3d::math::Point<f32>> for Point {
-    fn from(p: &ncollide3d::math::Point<f32>) -> Self {
-        use crate::uom::mm;
-        let x: Length = mm(p.x);
-        let y: Length = mm(p.y);
-        let z: Length = mm(p.z);
-        Self::new(x, y, z)
-    }
-}
-
-impl From<&ncollide3d::math::Vector<f32>> for Vector {
-    fn from(v: &ncollide3d::math::Vector<f32>) -> Self {
-        use crate::uom::mm;
-        let x: Length = mm(v.x);
-        let y: Length = mm(v.y);
-        let z: Length = mm(v.z);
-        Self::new(x, y, z)
-    }
-}
-
 impl From<&Point> for ncollide3d::math::Point<f32> {
     fn from(p: &Point) -> Self {
         use uom::si::length::millimeter as mm;
         let x = p.x.get::<mm>();
         let y = p.y.get::<mm>();
         let z = p.z.get::<mm>();
-        Self::new(x, y, z)
-    }
-}
-
-impl From<&Vector> for ncollide3d::math::Vector<f32> {
-    fn from(v: &Vector) -> Self {
-        use uom::si::length::millimeter as mm;
-        let x = v.x.get::<mm>();
-        let y = v.y.get::<mm>();
-        let z = v.z.get::<mm>();
         Self::new(x, y, z)
     }
 }
@@ -99,16 +69,6 @@ impl From<RatioVec> for ncollide3d::math::Vector<f32> {
 
 impl From<RatioPoint> for ncollide3d::math::Point<f32> {
     fn from(v: RatioPoint) -> Self {
-        use uom::si::ratio::ratio;
-        let x = v.x.get::<ratio>();
-        let y = v.y.get::<ratio>();
-        let z = v.z.get::<ratio>();
-        Self::new(x, y, z)
-    }
-}
-
-impl From<&RatioVec> for ncollide3d::math::Vector<f32> {
-    fn from(v: &RatioVec) -> Self {
         use uom::si::ratio::ratio;
         let x = v.x.get::<ratio>();
         let y = v.y.get::<ratio>();
