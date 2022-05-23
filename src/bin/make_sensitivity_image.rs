7507ba9816eb2d48b6e15b4eca31862c4843d80b
Jacek Generowicz <jacg@my-post-office.net>
2022-05-23 23:41:30 +0200

    Used different numbers of threads for lors & projections
    Because different numbers of threads are optimal in the two cases

--- src/bin/make_sensitivity_image.rs
+++ src/bin/make_sensitivity_image.rs
@@ -28,9 +28,13 @@ pub struct Cli {
     #[structopt(long)]
     pub rho: Lengthf32,
 
-    /// Maximum number of rayon threads
-    #[structopt(short = "j", long, default_value = "10")]
-    pub num_threads: usize,
+    /// Maximum number of rayon threads for LOR generation
+    #[structopt(short = "j", long, default_value = "20")]
+    pub lor_threads: usize,
+
+    /// Maximum number of rayon threads for image construction
+    #[structopt(short = "J", long, default_value = "8")]
+    pub image_threads: usize,
 
 }
 
@@ -49,7 +53,10 @@ use petalo::system_matrix as sm;
 
 fn main() -> Result<(), Box<dyn Error>> {
 
-    let Cli{ input, output, detector_length, detector_diameter, n_lors, rho, num_threads } = Cli::from_args();
+    let Cli {
+        input, output, detector_length, detector_diameter, n_lors, rho,
+        lor_threads, image_threads
+    } = Cli::from_args();
 
     // Set up progress reporting and timing
     use std::time::Instant;
@@ -73,14 +80,14 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     pre_report(&format!("Creating sensitivity image, using {} LORs ... ", group_digits(n_lors)))?;
 
-    let pool = rayon::ThreadPoolBuilder::new().num_threads(num_threads).build().unwrap();
+    let pool = rayon::ThreadPoolBuilder::new().num_threads(lor_threads).build().unwrap();
     let lors = pool.install(|| find_potential_lors(n_lors, density.fov, detector_length, detector_diameter));
     // TODO: Avoid creating a vector of LORs. Try to produce a rayon parallel
     // iterator in find_potential_lors and pass it for parallel processing to
     // sensitivity_image
     report_time("\nGenerated LORs");
 
-    let pool = rayon::ThreadPoolBuilder::new().num_threads(num_threads).build().unwrap();
+    let pool = rayon::ThreadPoolBuilder::new().num_threads(image_threads).build().unwrap();
     let sensitivity = pool.install(|| Image::sensitivity_image(density.fov, density, &lors, n_lors, rho));
     report_time("done (Calculated sensitivity image)");
 
