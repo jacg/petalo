5e3ad16536d02a9635211985baff4e476b0958bd
Jacek Generowicz <jacg@my-post-office.net>
2022-06-22 11:21:04 +0200

    Add basic example of config file parse

--- src/bin/parse_config.rs
+++ src/bin/parse_config.rs
@@ -1,29 +1,41 @@
+use std::error::Error;
+use std::fs;
+use std::path::PathBuf;
+
 use serde::Deserialize;
+use structopt::StructOpt;
 
-#[derive(Deserialize)]
-struct Config {
-    ip: String,
-    port: Option<u16>,
-    keys: Keys,
+#[derive(StructOpt, Debug, Clone)]
+struct Cli {
+    /// Configuration file
+    config_file: PathBuf,
 }
 
-#[derive(Deserialize)]
-struct Keys {
-    github: String,
-    travis: Option<String>,
+/// Toy configuration structure for MLEM. Just experimenting for now.
+#[derive(Deserialize, Debug)]
+pub struct Config {
+
+    /// Number of MLEM or OSEM iterations to perform
+    pub iterations: usize,
+
+    /// Number of OSEM subsets per iteration
+    pub subsets: usize,
+
 }
 
-fn main() {
-    let config: Config = toml::from_str(r#"
-        ip = '127.0.0.1'
 
-        [keys]
-        github = 'xxxxxxxxxxxxxxxxx'
-        travis = 'yyyyyyyyyyyyyyyyy'
-    "#).unwrap();
 
-    assert_eq!(config.ip, "127.0.0.1");
-    assert_eq!(config.port, None);
-    assert_eq!(config.keys.github, "xxxxxxxxxxxxxxxxx");
-    assert_eq!(config.keys.travis.as_ref().unwrap(), "yyyyyyyyyyyyyyyyy");
+fn main() -> Result<(), Box<dyn Error>> {
+
+    let args = Cli::from_args();
+
+
+    let config: String = fs::read_to_string(&args.config_file)
+        .expect(&format!("Couldn't read config file `{:?}`", args.config_file));
+
+    let config: Config = toml::from_str(&config)?;
+
+    println!("{config:?}");
+
+    Ok(())
 }
