59315475d4bfd85489c80834300b70a33b24487d
Jacek Generowicz <jacg@my-post-office.net>
2022-08-17 14:52:06 +0200

    Macro for uom wrapper functions

--- geometry/src/units.rs
+++ geometry/src/units.rs
@@ -63,32 +63,46 @@ pub mod mmps {
 //use uom::fmt::DisplayStyle::Abbreviation;
 pub use uom::si::Quantity;
 pub use mmps::f32::{Angle, TWOPI, Length, Time, Velocity, Ratio, Mass, PerLength, AreaPerMass};
-use uom::si::{length  ::{nanometer, millimeter, centimeter},
-              time    ::{nanosecond, picosecond},
-              mass    ::kilogram,
-              velocity:: meter_per_second};
-
+mod units {
+  pub use uom::si::{length  ::{nanometer, millimeter, centimeter},
+                    time    ::{nanosecond, picosecond},
+                    mass    ::kilogram,
+                    velocity::meter_per_second,
+                    ratio   ::ratio,
+                    angle   ::{radian, revolution},
+  };
+}
 // Making values from float literals seems to be very long-winded, so provide
 // some pithily-named convenience constructors. These would probably have to be
 // packed up in a constructor module in real life.
-pub fn cm (x: f32) -> Length   {  Length::new::      <centimeter>(x) }
-pub fn mm (x: f32) -> Length   {  Length::new::      <millimeter>(x) }
-pub fn nm (x: f32) -> Length   {  Length::new::      < nanometer>(x) }
-pub fn ns (x: f32) -> Time     {    Time::new::      <nanosecond>(x) }
-pub fn ps (x: f32) -> Time     {    Time::new::      <picosecond>(x) }
-pub fn m_s(x: f32) -> Velocity {Velocity::new::<meter_per_second>(x) }
-pub fn kg (x: f32) -> Mass     {    Mass::new::        <kilogram>(x) }
 
-pub fn ratio (x: f32) -> Ratio  {   Ratio::new::<uom::si::ratio::ratio>(x) }
-pub fn radian(x: f32) -> Angle  {   Angle::new::<uom::si::angle::radian>(x) }
-pub fn turn  (x: f32) -> Angle  {   Angle::new::<uom::si::angle::revolution>(x) }
+/// Generate a function called NAME which returns QUANTITY by interpreting its
+/// argument as UNIT
+///
+/// wrap!(NAME QUANTITY UNIT);
+macro_rules! wrap {
+  ($name:ident $quantity:ident $unit:ident ) => {
+    pub fn $name(x: f32) -> $quantity { $quantity::new::<units::$unit>(x) }
+  };
+}
+
+wrap!(cm     Length         centimeter);
+wrap!(mm     Length         millimeter);
+wrap!(nm     Length          nanometer);
+wrap!(ns     Time           nanosecond);
+wrap!(ps     Time           picosecond);
+wrap!(m_s    Velocity meter_per_second);
+wrap!(kg     Mass             kilogram);
+wrap!(ratio  Ratio               ratio);
+wrap!(radian Angle              radian);
+wrap!(turn   Angle          revolution);
 
 // Reverse direction of the above. Rethink nomenclature once the dust has
 // settled after the transition to uom is complete.
-pub fn mm_(x: Length) -> f32 { x.get::<millimeter>() }
-pub fn ps_(x: Time  ) -> f32 { x.get::<picosecond>() }
-pub fn ns_(x: Time  ) -> f32 { x.get::<nanosecond>() }
-pub fn kg_(x: Mass  ) -> f32 { x.get::<kilogram>  () }
+pub fn mm_(x: Length) -> f32 { x.get::<units::millimeter>() }
+pub fn ps_(x: Time  ) -> f32 { x.get::<units::picosecond>() }
+pub fn ns_(x: Time  ) -> f32 { x.get::<units::nanosecond>() }
+pub fn kg_(x: Mass  ) -> f32 { x.get::<units::kilogram>  () }
 
 pub fn ratio_ (x: Ratio) -> f32 { x.get::<uom::si::ratio::ratio>() }
 pub fn radian_(x: Angle) -> f32 { x.get::<uom::si::angle::radian>() }
@@ -96,7 +110,7 @@ pub fn turn_  (x: Angle) -> f32 { x.get::<uom::si::angle::revolution>() }
 
 pub fn mm_ps (x: f32) -> Velocity { m_s (x / m_s(1.0).value) }
 pub fn mm_ps_(x: Velocity) -> f32 { m_s_(x * m_s(1.0).value) }
-pub fn m_s_  (x: Velocity) -> f32 { x.get::<meter_per_second>() }
+pub fn m_s_  (x: Velocity) -> f32 { x.get::<units::meter_per_second>() }
 
 #[macro_export]
 macro_rules! in_base_unit {
@@ -129,6 +143,7 @@ mod tests {
   fn test_name() {
     let v = vec![mm(1.0), cm(1.0)];
     let total: Length = v.into_iter().sum();
+    use units::nanometer;
     assert_uom_eq!(nanometer, total, mm(11.0), ulps <= 1);
   }
 }
