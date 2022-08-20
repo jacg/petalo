a79dafa56b4a0fe58327f0c78cf4ae14ff96612f
Jacek Generowicz <jacg@my-post-office.net>
2022-08-20 15:02:08 +0200

    Tidy up exports.rs

--- src/exports.rs
+++ src/exports.rs
@@ -3,13 +3,11 @@ pub use crate::system_matrix::{LOR, find_tof_peak, find_entry_point, voxel_size,
 use ncollide3d as nc;
 use units::todo::Lengthf32;
 
-pub use geometry::RatioVec;
-pub type Vectorf32 = nc::math::Vector<Lengthf32>;
-pub use geometry::Vector;
+pub use geometry::{Vector, RatioPoint, RatioVec};
 
-pub use geometry::RatioPoint;
-pub type Pointf32 = nc::math::Point <Lengthf32>;
-pub type Point    = geometry::Point;
+pub type Vectorf32 = nc::math::Vector<Lengthf32>;
+pub type Pointf32  = nc::math::Point <Lengthf32>;
+pub type Point     = geometry::Point;
 
 pub use crate::index::{BoxDim_u, Index1_u, Index3_u, Index3Weightf32, LengthI, LengthU};
 
