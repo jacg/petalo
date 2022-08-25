da24f9f039478b62f4bc53e57ac3f7f11f4f4c6e
Jacek Generowicz <jacg@my-post-office.net>
2022-08-25 20:48:57 +0200

    Fix /// -> ///! units::todo module

--- units/src/todo.rs
+++ units/src/todo.rs
@@ -1,16 +1,16 @@
-/// Units which are simply type aliases for `f32` rather than having an
-/// implementation as a `uom` `Quantity`.
-///
-/// This may be because:
-///
-/// + We do not know how to implement them in `uom`.
-///
-/// + There are other complications in the client code which make using `uom`
-///   difficult, so we use plain `f32`s, but still want some clues in the source
-///   as to what they represent.
-///
-/// + They might not be difficult to implement, but we haven't had the time to
-///   think about it.
+//! Units which are simply type aliases for `f32` rather than having an
+//! implementation as a `uom` `Quantity`.
+//!
+//! This may be because:
+//!
+//! + We do not know how to implement them in `uom`.
+//!
+//! + There are other complications in the client code which make using `uom`
+//!   difficult, so we use plain `f32`s, but still want some clues in the source
+//!   as to what they represent.
+//!
+//! + They might not be difficult to implement, but we haven't had the time to
+//!   think about it.
 
 pub type Lengthf32    = f32;
 pub type Timef32      = f32;
