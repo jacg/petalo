f6fda1878577ae50f8cd195a0ee1895b769fb970
Jacek Generowicz <jacg@my-post-office.net>
2023-03-22 16:40:19 +0100

    WIP

--- src/bin/makelor.rs
+++ src/bin/makelor.rs
@@ -114,6 +114,22 @@ fn main() -> hdf5::Result<()> {
     // --- Process input files -------------------------------------------------------
     let xyzs = io::hdf5::sensors::read_sensor_map(&args.infiles[0])?;
 
+
+    let files = args.infiles.clone();
+    let lors: Vec<_> = match &args.reco {
+        d@Reco::Discrete { .. } => xxx(files, read_vertices, group_vertices, lor_from_discretized_vertices(d)),
+        Reco::FirstVertex       => xxx(files, read_vertices, group_vertices, lor_from_first_vertices),
+        Reco::BaryVertex        => xxx(files, read_vertices, group_vertices, lor_from_barycentre_of_vertices),
+        &Reco::Half { q }       => xxx(files, read_qts     , group_qts(q) ,  lor_from_hits(&xyzs)),
+        &Reco::Dbscan { q, min_count, max_distance } =>
+            xxx(files, read_qts, group_qts(q), lor_from_hits_dbscan_NEW(&xyzs, min_count, max_distance)),
+        Reco::SimpleRec { .. } => todo!(),
+    };
+
+    println!("Found {} lors", lors.len());
+    todo!();
+
+
     let makelors = make_makelors_fn(&args, &xyzs);
 
     #[derive(Default)]
@@ -175,7 +191,49 @@ fn main() -> hdf5::Result<()> {
     Ok(())
 }
 
-type FilenameToLorsFunction<'a> = Box<dyn Fn(&PathBuf) -> hdf5::Result<LorBatch> + 'a + Sync>;
+pub fn yyy<T>(
+    events: &[Vec<T>],
+    mut one_lor: impl FnMut(&[T]) -> Option<Hdf5Lor>
+) -> Vec<Hdf5Lor> {
+    events.iter()
+        .flat_map(|data| one_lor(data))
+        .collect()
+}
+
+fn xxx<T>(
+    files: Vec<PathBuf>,
+    extract_stuff_from_file: impl Fn(&Path)  -> hdf5::Result<Vec<T>> + Send + Sync,
+    group_stuff            : impl Fn(Vec<T>) -> Vec<Vec<T>>          + Send + Sync,
+    make_one_lor           : impl Fn(  &[T]) -> Option<Hdf5Lor>,
+) -> Vec<Hdf5Lor> {
+    files
+        .into_iter()
+        .map(move |file| extract_stuff_from_file(&file).unwrap_or_else(|e| -> Vec<T>{
+            dbg!(e);
+            vec![]
+        }))
+        .flat_map(move |stuff| group_stuff(stuff).into_iter())
+        .filter_map(|x| make_one_lor(&x))
+        .collect()
+}
+
+fn group_vertices(vertices: Vec<Vertex>) -> Vec<Vec<Vertex>> {
+    group_by(|v| v.event_id, vertices)
+}
+
+fn group_qts(q: u32) -> impl Fn(Vec<QT>) -> Vec<Vec<QT>> {
+    move |qts| {
+        qts.into_iter()
+           .filter(|h| h.q >= q)
+           .group_by(|h| h.event_id)
+           .into_iter()
+           .map(|(_, group)| group.collect())
+           .collect()
+    }
+}
+
+type FilenameToGroupsFunction<'a, T> = Box<dyn Fn(&PathBuf) -> hdf5::Result<Vec<Vec<T>>> + 'a + Sync>;
+type FilenameToLorsFunction  <'a>    = Box<dyn Fn(&PathBuf) -> hdf5::Result<LorBatch>    + 'a + Sync>;
 
 fn make_makelors_fn<'xyzs>(args: &Cli, xyzs: &'xyzs SensorMap) -> FilenameToLorsFunction<'xyzs> {
     match &args.reco {
@@ -185,14 +243,14 @@ fn make_makelors_fn<'xyzs>(args: &Cli, xyzs: &'xyzs SensorMap) -> FilenameToLors
 
         &Reco::Half{q} => Box::new(
             move |infile: &PathBuf| -> hdf5::Result<LorBatch> {
-                let events = group_qts(q, infile)?;
-                Ok(LorBatch::new(lors_from(&events, |evs| lor_from_hits(evs, xyzs)), events.len()))
+                let events = group_qts_OLD(q, infile)?;
+                Ok(LorBatch::new(lors_from(&events, |evs| lor_from_hits_OLD(evs, xyzs)), events.len()))
             }),
 
         &Reco::Dbscan { q, min_count, max_distance } => Box::new(
             move |infile: &PathBuf| -> hdf5::Result<LorBatch> {
-                let events = group_qts(q, infile)?;
-                Ok(LorBatch::new(lors_from(&events, |evs| lor_from_hits_dbscan(evs, xyzs, min_count, max_distance)), events.len()))
+                let events = group_qts_OLD(q, infile)?;
+                Ok(LorBatch::new(lors_from(&events, |evs| lor_from_hits_dbscan_OLD(evs, xyzs, min_count, max_distance)), events.len()))
             }),
 
         &Reco::SimpleRec { pde, sigma_t, threshold, nsensors, charge_limits }
@@ -202,7 +260,7 @@ fn make_makelors_fn<'xyzs>(args: &Cli, xyzs: &'xyzs SensorMap) -> FilenameToLors
 
 fn from_vertices(lor_from_vertices: impl Fn(&[Vertex]) -> Option<Hdf5Lor> + 'static + Copy) -> impl Fn(&PathBuf) -> hdf5::Result<LorBatch> + 'static {
     move |infile: &PathBuf| -> hdf5::Result<LorBatch> {
-        let events = group_vertices(infile)?;
+        let events = group_vertices_OLD(infile)?;
         Ok(LorBatch::new(lors_from(&events, lor_from_vertices), events.len()))
     }
 }
@@ -210,13 +268,13 @@ fn from_vertices(lor_from_vertices: impl Fn(&[Vertex]) -> Option<Hdf5Lor> + 'sta
 fn read_vertices(infile: &Path) -> hdf5::Result<Vec<Vertex>> { io::hdf5::mc::read_vertices(infile, Bounds::none()) }
 fn read_qts     (infile: &Path) -> hdf5::Result<Vec<  QT  >> { io::hdf5::sensors::read_qts(infile, Bounds::none()) }
 
-fn group_vertices(infile: &Path) -> hdf5::Result<Vec<Vec<Vertex>>> {
+fn group_vertices_OLD(infile: &Path) -> hdf5::Result<Vec<Vec<Vertex>>> {
     Ok(group_by(|v| v.event_id,
                 read_vertices(infile)?
                 .into_iter()))
 }
 
-fn group_qts(q: u32, infile: &Path) -> hdf5::Result<Vec<Vec<  QT  >>> {
+fn group_qts_OLD(q: u32, infile: &Path) -> hdf5::Result<Vec<Vec<  QT  >>> {
     Ok(group_by(|h| h.event_id,
                 read_qts(infile)?
                 .into_iter()
@@ -373,7 +431,24 @@ fn lor_from_barycentre_of_vertices(vertices: &[Vertex]) -> Option<Hdf5Lor> {
     })
 }
 
-fn lor_from_hits(hits: &[QT], xyzs: &SensorMap) -> Option<Hdf5Lor> {
+fn lor_from_hits(xyzs: &SensorMap) -> impl Fn(&[QT]) -> Option<Hdf5Lor> + '_ {
+    move |hits| {
+        let (cluster_a, cluster_b) = group_into_clusters(hits, xyzs)?;
+        //println!("{} + {} = {} ", cluster_a.len(), cluster_b.len(), hits.len());
+        let (p1, t1) = cluster_xyzt(&cluster_a, xyzs)?;
+        let (p2, t2) = cluster_xyzt(&cluster_b, xyzs)?;
+        //println!("{:?} {:?}", xyzt_a, xyzt_b);
+        Some(Hdf5Lor {
+            dt: ns_(t2 - t1),
+            x1: mm_(p1.x), y1: mm_(p1.y), z1: mm_(p1.z),
+            x2: mm_(p2.x), y2: mm_(p2.y), z2: mm_(p2.z),
+            // TODO qs and Es missing
+            q1: f32::NAN, q2: f32::NAN,   E1: f32::NAN, E2: f32::NAN,
+        })
+    }
+}
+
+fn lor_from_hits_OLD(hits: &[QT], xyzs: &SensorMap) -> Option<Hdf5Lor> {
     let (cluster_a, cluster_b) = group_into_clusters(hits, xyzs)?;
     //println!("{} + {} = {} ", cluster_a.len(), cluster_b.len(), hits.len());
     let (p1, t1) = cluster_xyzt(&cluster_a, xyzs)?;
@@ -407,7 +482,51 @@ mod test_n_clusters {
     }
 }
 
-fn lor_from_hits_dbscan(hits: &[QT], xyzs: &SensorMap, min_points: usize, tolerance: Length) -> Option<Hdf5Lor> {
+fn lor_from_hits_dbscan_NEW(
+    xyzs: &SensorMap,
+    min_points: usize,
+    tolerance: Length
+) -> impl Fn(&[QT]) -> Option<Hdf5Lor> + '_ {
+    use linfa_clustering::AppxDbscan;
+    use linfa::traits::Transformer;
+    move |hits| {
+        let active_sensor_positions: ndarray::Array2<f32> = hits.iter()
+            .flat_map(|QT { sensor_id, ..}| xyzs.get(sensor_id))
+            .map(|&(x,y,z)| [mm_(x), mm_(y), mm_(z)])
+            .collect::<Vec<_>>()
+            .into();
+        let params = AppxDbscan::params(min_points)
+            .tolerance(mm_(tolerance)); // > 7mm between sipm centres
+        let labels = params.transform(&active_sensor_positions).ok()?;
+        let n_clusters = count_clusters(&labels);
+        if n_clusters != 2 { return None }
+        let mut cluster: [Vec<f32>; 2] = [vec![], vec![]];
+        for (c, point) in labels.iter().zip(active_sensor_positions.outer_iter()) {
+            if let Some(c) = c {
+                cluster[*c].extend(point);
+            }
+        }
+        fn cluster_centroid(vec: Vec<f32>) -> Option<ndarray::Array1<f32>> {
+            let it = ndarray::Array2::from_shape_vec((vec.len()/3, 3), vec.clone()).unwrap()
+                .mean_axis(ndarray::Axis(0));
+            if it.is_none() {
+                println!("Failed to find centroid of cluster {:?}", vec);
+            }
+            it
+        }
+        let a = cluster_centroid(cluster[0].clone())?;
+        let b = cluster_centroid(cluster[1].clone())?;
+        Some(Hdf5Lor {
+            dt: 0.0, // TODO dt missing
+            x1: a[0], y1: a[1], z1: a[2],
+            x2: b[0], y2: b[1], z2: b[2],
+            // TODO qs and Es missing
+            q1: f32::NAN, q2: f32::NAN,   E1: f32::NAN, E2: f32::NAN,
+        })
+    }
+}
+
+fn lor_from_hits_dbscan_OLD(hits: &[QT], xyzs: &SensorMap, min_points: usize, tolerance: Length) -> Option<Hdf5Lor> {
     use linfa_clustering::AppxDbscan;
     use linfa::traits::Transformer;
     let active_sensor_positions: ndarray::Array2<f32> = hits.iter()
