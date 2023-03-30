59b78a865c92630514859c8e9fccc8a5aa2b071f
Jacek Generowicz <jacg@my-post-office.net>
2023-03-30 15:33:06 +0200

    Move make_sensitivity_image Cli into separate mod

---
+++ src/bin/make_sensitivity_image/cli.rs
@@ -0,0 +1,64 @@
+#[derive(clap::Parser, Debug, Clone)]
+#[clap(name = "make_sensitivity_image", about = "Create sensitivity image from a density image")]
+pub struct Cli {
+
+    /// The density image of the FOV
+    #[clap(short, long)]
+    pub input: PathBuf,
+
+    /// Where to write the resulting sensitivity image
+    #[clap(short, long)]
+    pub output: Option<PathBuf>,
+
+    /// Detector length for sensitivity image generation
+    #[clap(long, short='l')]
+    pub detector_length: Length,
+
+    /// Inner radius of scintillator for sensitivity image generation
+    #[clap(long, short='r')]
+    pub r_min: Length,
+
+    /// Conversion from density to attenuation coefficient in cm^2 / g
+    #[clap(long, default_value = "0.095")]
+    pub rho_to_mu: Lengthf32,
+
+    /// Maximum number of rayon threads
+    #[clap(short = 'j', long, default_value = "30")]
+    pub n_threads: usize,
+
+    #[clap(subcommand)]
+    pub detector_type: DetectorType,
+}
+
+#[derive(clap::Parser, Debug, Clone)]
+pub enum DetectorType {
+
+    /// Continuous scintillator
+    Continuous {
+        /// Number of randomly-generated LORs to use
+        n_lors: usize,
+    },
+
+    /// Discretize scintillator
+    Discrete {
+
+        /// Radial size of elements = thickness of scintillator
+        #[clap(long)]
+        dr: Length,
+
+        /// Axial width of scintillator elements
+        #[clap(long)]
+        dz: Length,
+
+        /// Azimuthal width of scintillator elements at `r_min + dr/2`?
+        #[clap(long)]
+        da: Length,
+    },
+}
+
+// ----- Imports -----------------------------------------------------------------------------------------
+use std::path::PathBuf;
+
+use units::{
+    Length, todo::Lengthf32,
+};
--- src/bin/make_sensitivity_image/main.rs
+++ src/bin/make_sensitivity_image/main.rs
@@ -1,61 +1,7 @@
-// ----------------------------------- CLI -----------------------------------
-#[derive(clap::Parser, Debug, Clone)]
-#[clap(name = "make_sensitivity_image", about = "Create sensitivity image from a density image")]
-pub struct Cli {
-
-    /// The density image of the FOV
-    #[clap(short, long)]
-    pub input: PathBuf,
-
-    /// Where to write the resulting sensitivity image
-    #[clap(short, long)]
-    pub output: Option<PathBuf>,
-
-    /// Detector length for sensitivity image generation
-    #[clap(long, short='l')]
-    pub detector_length: Length,
-
-    /// Inner radius of scintillator for sensitivity image generation
-    #[clap(long, short='r')]
-    pub r_min: Length,
-
-    /// Conversion from density to attenuation coefficient in cm^2 / g
-    #[clap(long, default_value = "0.095")]
-    pub rho_to_mu: Lengthf32,
-
-    /// Maximum number of rayon threads
-    #[clap(short = 'j', long, default_value = "30")]
-    pub n_threads: usize,
-
-    #[clap(subcommand)]
-    detector_type: DetectorType,
-}
-
-#[derive(clap::Parser, Debug, Clone)]
-enum DetectorType {
-
-    /// Continuous scintillator
-    Continuous {
-        /// Number of randomly-generated LORs to use
-        n_lors: usize,
-    },
-
-    /// Discretize scintillator
-    Discrete {
-
-        /// Radial size of elements = thickness of scintillator
-        #[clap(long)]
-        dr: Length,
-
-        /// Axial width of scintillator elements
-        #[clap(long)]
-        dz: Length,
-
-        /// Azimuthal width of scintillator elements at `r_min + dr/2`?
-        #[clap(long)]
-        da: Length,
-    },
-}
+mod discrete;
+mod continuous;
+mod cli;
+use cli::*;
 
 fn main() -> Result<(), Box<dyn Error>> {
 
@@ -144,12 +90,7 @@ fn density_image_into_attenuation_image(density: Image, rho_to_mu: AreaPerMass) 
 
 }
 
-mod discrete;
-mod continuous;
-
 // ----- Imports -----------------------------------------------------------------------------------------
-use clap::Parser;
-
 use std::{
     error::Error,
     io::Write,
@@ -164,5 +105,6 @@ use petalo::{
 };
 
 use units::{
-    Length, AreaPerMass, ratio_, kg, mm, todo::Lengthf32,
+    AreaPerMass, ratio_, kg, mm,
 };
+use clap::Parser;
