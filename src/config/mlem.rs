52fcbf12d6ee285328b2067aa3029b5a81bf12af
Jacek Generowicz <jacg@my-post-office.net>
2023-04-25 15:17:01 +0200

    Move E-smearing to input section of config file

--- src/config/mlem.rs
+++ src/config/mlem.rs
@@ -8,7 +8,6 @@ use serde::{Deserialize, Deserializer, de};
 
 use units::{Length, Ratio, Time};
 
-#[cfg(test)]
 fn deserialize_uom_opt<'d, D, T>(deserializer: D) -> Result<Option<T>, D::Error>
 where
     D: Deserializer<'d>,
@@ -92,9 +91,6 @@ pub struct Config {
     /// Sensitivity image to use in scatter correction
     pub attenuation_correction: Option<AttenuationCorrection>,
 
-    /// Smearing to apply to observed energy
-    pub smear_energy: Option<SmearEnergy>,
-
     pub scatter_correction: Option<Scatter>,
 }
 
@@ -118,6 +114,9 @@ pub struct Input {
 
     #[serde(default)]
     pub events: Bounds<usize>,
+
+    #[serde(default, deserialize_with = "deserialize_uom_opt")]
+    pub e_smear_fwhm: Option<Ratio>,
 
 }
 
@@ -227,13 +226,6 @@ pub struct BinsLength {
     pub bins: usize,
     #[serde(deserialize_with = "deserialize_uom")]
     pub length: Length,
-}
-
-#[derive(Deserialize, Debug, Clone, Default)]
-#[serde(deny_unknown_fields)]
-pub struct SmearEnergy {
-    #[serde(deserialize_with = "deserialize_uom")]
-    pub fwhm: Ratio,
 }
 
 pub fn read_config_file(path: PathBuf) -> Config {
@@ -605,16 +597,17 @@ mod tests {
     fn toml_with_units_generic_deserialize() {
         #[derive(Deserialize, Debug)]
         struct X {
-            #[serde(default)]
-            #[serde(deserialize_with = "deserialize_uom_opt")]
+            #[serde(default, deserialize_with = "deserialize_uom_opt")]
             t: Option<Time>,
-            #[serde(default)]
-            #[serde(deserialize_with = "deserialize_uom_opt")]
+            #[serde(default, deserialize_with = "deserialize_uom_opt")]
             l: Option<Length>,
+            #[serde(default, deserialize_with = "deserialize_uom_opt")]
+            r: Option<Ratio>,
         }
         check!(X(r#"t = "2 ps""#).t = Some(ps(2.)));
         check!(X(r#"l = "3 mm""#).l = Some(mm(3.)));
         check!(X(r#"          "#).t = None        );
+        check!(X(r#"r = "25 %""#).r = Some(ratio(0.25)));
     }
 
 }
