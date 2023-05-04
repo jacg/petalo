445d576ecaedad7783671f5e8e7c92d8bf9fd98f
Jacek Generowicz <jacg@my-post-office.net>
2023-05-04 17:53:10 +0200

    Replace {read -> iter}_dataset in read_hdf5_lors

--- src/io/hdf5.rs
+++ src/io/hdf5.rs
@@ -100,8 +100,7 @@ fn read_hdf5_lors(config: &Config) -> Result<(Vec<Hdf5Lor>, usize), Box<dyn Erro
     let z_max = config.detector_full_axial_length.map(|l| mm_(l.dz / 2.0));
     // Read LOR data from disk
     let hdf5_lors: Vec<Hdf5Lor> = {
-        read_dataset::<Hdf5Lor>(&input.file, &input.dataset, input.events.clone())?
-            .iter().cloned()
+        iter_dataset::<Hdf5Lor>(&input.file, &input.dataset, input.events.clone())?
             .filter(|&Hdf5Lor{E1, E2, q1, q2, z1, z2, ..}| {
                 let eok = input.energy.contains(E1) && input.energy.contains(E2);
                 let qok = input.charge.contains(q1) && input.charge.contains(q2);
