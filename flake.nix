60eb27376c9efdf797ea2a1aee546728ae36943a
Jacek Generowicz <jacg@my-post-office.net>
2023-06-12 10:23:29 +0200

    See if MacOS has been fixed on nixos-unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Image Reconstruction for PET";
 
   inputs = {
-    nixpkgs         .url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs         .url = "github:nixos/nixpkgs/nixos-unstable";
     oldpkgs         .url = "github:nixos/nixpkgs/nixos-22.11";
     utils           .url = "github:numtide/flake-utils";
     rust-overlay = { url = "github:oxalica/rust-overlay"; inputs.nixpkgs    .follows = "nixpkgs";
